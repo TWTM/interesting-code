7513b2e4139004e8301d784eaaba5143149d65e9
TWTM <52894737+TWTM@users.noreply.github.com>
2022-05-16 20:44:31 -0300

    Update disease_simulation_SIR.py

--- disease_simulation_SIR.py
+++ disease_simulation_SIR.py
@@ -2,10 +2,11 @@ from matplotlib import pyplot as plt
 import numpy as np
 import time
 
+tempo = time.time()
 
 # Criando uma pessoa que contem o estado dela (saudavel ou gripada) e a quantidade de dias que ela esta gripada
 class person():
-    def __init__(self, state, days, immunity, location_x, location_y, id) -> None:
+    def __init__(self, state, days, immunity, location_x, location_y,id) -> None:
         self.id = id
         self.state = state
         self.days = days
@@ -13,12 +14,13 @@ class person():
         self.location_x = location_x
         self.location_y = location_y
 
-radius = 2
-days_until_healed = 6
-time_to_spread = 3
-initial_prob = 0.1
-total_days = []
-
+def printgrid():
+    for i in healthy_people:
+        plt.scatter(i.location_x, i.location_y, color = 'green', s = 5)
+    for x in infected_people:
+        plt.scatter(x.location_x, x.location_y, color = 'red', s = 5)
+    plt.show()
+    print()
 
 def draw():
     fig = plt.figure(figsize=(10,8))
@@ -31,53 +33,55 @@ def draw():
     leg = axes_1.legend()
     plt.show()
 
-people = []
+    
+radius = 20
+days_until_healed = 6
+time_to_spread = 3
+initial_prob = 0.1
+total_days = []
 infected_people = []
 healthy_people = []
-# Criando a sala
-for x in range(2500):
-    random_x = np.random.randint(0,50)
-    random_y = np.random.randint(0,50)
-    if x % 20 == 0:
-        infected_people.append(person('G', 0, 1, random_x ,random_y, x))
-    else:
-        healthy_people.append(person('S', 0, 1, random_x ,random_y, x))
-        
-
-def printgrid():
-    for i in healthy_people:
-        plt.scatter(i.location_x, i.location_y, color = 'green', s = 5)
-    for x in infected_people:
-        plt.scatter(x.location_x, x.location_y, color = 'red', s = 5)
-    plt.show()
-    print()
-
-printgrid()
-
-
 days = 0
-nearby = []
-n_infected = len(infected_people)
-n_healthy = len(healthy_people)
+
+# Used to draw the graph
+n_infected = 0
+n_healthy = 0
 n_recovered = 0
 recovered = []
 infected = []
 healthy = []
 
+# Criando a sala
+for x in range(20000):
+    random_x = np.random.uniform(0,1000)
+    random_y = np.random.uniform(0,1000)
+    if x % 250 == 0:
+        infected_people.append(person('G', 0, 1, random_x ,random_y,x))
+    else:
+        healthy_people.append(person('S', 0, 1, random_x ,random_y,x))
+
+sorted(healthy_people, key=lambda x: x.location_x)
+sorted(infected_people, key=lambda x: x.location_x)
+
+
 while (len(infected_people) != 0):
-    # Correr cada lugar da sala e ver se ta contaminado
+# Correr cada lugar da sala e ver se ta contaminado
     for x in infected_people:
         if x.days >= time_to_spread:
             # Pegando o lugar das pessoas que possivelmente possam ser contaminadas
             for i in healthy_people:
-                if ((i.location_x - x.location_x)**2 + (i.location_x - x.location_x)**2 <= radius**2):
-                    chance_of_infection = np.random.uniform(0,1)
-                    # 30% de chance de ser infectado
-                    if chance_of_infection <= initial_prob * 1/i.immunity:
-                        i.state = 'G'
-                        healthy_people.remove(i)
-                        infected_people.append(i)
-
+                if i.location_x > x.location_x + radius:
+                    continue
+                elif i.location_x < x.location_x - radius:
+                    continue
+                else:
+                    if ((i.location_x - x.location_x)**2 + (i.location_y - x.location_y)**2 <= radius**2):
+                        chance_of_infection = np.random.uniform(0,1)
+                        # 30% de chance de ser infectado
+                        if chance_of_infection <= initial_prob * 1/i.immunity:
+                            i.state = 'G'
+                            healthy_people.remove(i)
+                            infected_people.append(i)
 
     # Checking the days since he got the desease
     for i in infected_people:
@@ -91,14 +95,19 @@ while (len(infected_people) != 0):
             i.immunity += 1
             infected_people.remove(i)
             healthy_people.append(i)
-            if i.immunity == 3:
+            if i.immunity == 5:
                 i.state = 'R'
                 n_recovered+=1
                 healthy_people.remove(i)
     recovered.append(n_recovered)
     days += 1              
     total_days.append(days)
+
     infected.append(len(infected_people))
     healthy.append(len(healthy_people))
+    sorted(healthy_people, key=lambda x: x.location_x)
+    sorted(infected_people, key=lambda x: x.location_x)
+
 print(days)
+print(time.time() - tempo)
 draw()
