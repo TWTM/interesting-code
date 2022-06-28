a9af44d4cbce4d061f3d2429375ad7ca1b75e5d4
TWTM <52894737+TWTM@users.noreply.github.com>
2022-06-28 19:21:18 -0300

    Update disease_simulation_SIR.py

--- disease_simulation_SIR.py
+++ disease_simulation_SIR.py
@@ -4,43 +4,12 @@ import time
 
 tempo = time.time()
 
-# Criando uma pessoa que contem o estado dela (saudavel ou gripada) e a quantidade de dias que ela esta gripada
-class person():
-    def __init__(self, state, days, immunity, location_x, location_y,id) -> None:
-        self.id = id
-        self.state = state
-        self.days = days
-        self.immunity = immunity
-        self.location_x = location_x
-        self.location_y = location_y
-
-def printgrid():
-    for i in healthy_people:
-        plt.scatter(i.location_x, i.location_y, color = 'green', s = 5)
-    for x in infected_people:
-        plt.scatter(x.location_x, x.location_y, color = 'red', s = 5)
-    plt.show()
-    print()
-
-def draw():
-    fig = plt.figure(figsize=(10,8))
-    axes_1 = fig.add_axes([0.1,0.1,0.9,0.9])
-    axes_1.set_ylabel('Number of infected people in the classroom')
-    axes_1.set_xlabel('Days')
-    axes_1.plot(total_days, infected, label='Infected',color = 'red')
-    axes_1.plot(total_days, recovered, label='Recovered',color = 'blue')
-    axes_1.plot(total_days, healthy, label='Healthy',color = 'Green')
-    leg = axes_1.legend()
-    plt.show()
-
-global n_infected, n_healthy, recovered ,infected ,healthy, total_days, infected_people, healthy_people,days
-
 # Changeable Parameters
-total_people = 7000
-radius = 70
+total_people = 1000
+radius = 8
 days_until_healed = 6
 time_to_spread = 3
-initial_prob = 0.1
+initial_prob = 0.15
 
 # Used to draw the graph
 n_infected = 0
@@ -54,23 +23,75 @@ healthy = []
 total_days = []
 infected_people = []
 healthy_people = []
+recovered_people = []
+population = [healthy_people, infected_people, recovered_people]
 days = 0
+
+# Criando uma pessoa que contem o estado dela (saudavel ou gripada) e a quantidade de dias que ela esta gripada
+class person():
+    def __init__(self, state, days, immunity, location_x, location_y,id) -> None:
+        self.id = id
+        self.state = state
+        self.days = days
+        self.immunity = immunity
+        self.location_x = location_x
+        self.location_y = location_y
+
+def printgrid():
+    infected_y = []
+    infected_x = []
+    healthy_y = []
+    healthy_x = []
+    recovered_x = []
+    recovered_y = []
+    for i in infected_people:
+        infected_x.append(i.location_x)
+        infected_y.append(i.location_y)
+    for h in healthy_people:
+        healthy_x.append(h.location_x)
+        healthy_y.append(h.location_y)
+    for r in recovered_people:
+        recovered_x.append(r.location_x)
+        recovered_y.append(r.location_y)
+    
+    for i in range(3):
+        if i == 0:
+            plt.scatter(healthy_x, healthy_y, color = 'green', s = 3)
+            plt.pause(1)
+        elif i == 1:
+            plt.scatter(infected_x, infected_y, color = 'red', s = 3)
+            plt.pause(1)
+        else:
+            plt.scatter(recovered_x, recovered_y, color = 'blue', s = 3)
+            plt.pause(0.2)
+    plt.title(days)
+    plt.xlabel(len(infected_people))
+
+
+def draw():
+    fig = plt.figure(figsize=(10,8))
+    axes_1 = fig.add_axes([0.1,0.1,0.9,0.9])
+    axes_1.set_ylabel('Number of infected people in the classroom')
+    axes_1.set_xlabel('Days')
+    axes_1.plot(total_days, infected, label='Infected',color = 'red')
+    axes_1.plot(total_days, recovered, label='Recovered',color = 'blue')
+    axes_1.plot(total_days, healthy, label='Healthy',color = 'green')
+    leg = axes_1.legend()
+    plt.show()
 
 # Criando a sala
 def create_room():
+    global infected_people, healthy_people
     for x in range(total_people):
-                                      #size of the square
-        random_x = np.random.uniform(0,total_people*0.15)
-        random_y = np.random.uniform(0,total_people*0.15)
-        if x % int(7000*0.1) == 0:
-            #creating infected people
+        random_x = np.random.uniform(0,total_people*0.1)
+        random_y = np.random.uniform(0,total_people*0.1)
+        if x % 10 == 0:
             infected_people.append(person('G', 0, 1, random_x ,random_y,x))
-        else:
-            healthy_people.append(person('S', 0, 1, random_x ,random_y,x))
+            continue    
+        healthy_people.append(person('S', 0, 1, random_x ,random_y,x))
 
-    # sorting healthy and infected people based on their X position
-    sorted(healthy_people, key=lambda x: x.location_x)
-    sorted(infected_people, key=lambda x: x.location_x)
+    healthy_people = sorted(healthy_people, key=lambda x: x.location_x)
+    infected_people = sorted(infected_people, key=lambda x: x.location_x)
 
 def healing():
     global n_infected, n_healthy, n_recovered, recovered ,infected ,healthy, total_days, infected_people, healthy_people, days
@@ -86,25 +107,29 @@ def healing():
             i.immunity += 1
             infected_people.remove(i)
             healthy_people.append(i)
-            # if he has been infected 5 times he now is categorized as immune and cant be infected again
-            if i.immunity == 5:
+            # if he has been infected 3 times he now is categorized as immune and cant be infected again
+            if i.immunity == 3:
                 i.state = 'R'
                 n_recovered += 1
                 healthy_people.remove(i)
-    
-    # appending for graph creation purposes
+                recovered_people.append(i)
+
     recovered.append(n_recovered)
+    
     days += 1              
     total_days.append(days)
     infected.append(len(infected_people))
     healthy.append(len(healthy_people))
-    # sorting 
-    sorted(healthy_people, key=lambda x: x.location_x)
-    sorted(infected_people, key=lambda x: x.location_x)
+
+    if days % 3 == 0:
+            printgrid()
+
+    healthy_people = sorted(healthy_people, key=lambda x: x.location_x)
+    infected_people = sorted(infected_people, key=lambda x: x.location_x)
 
 def infecting():
     while (len(infected_people) != 0):
-    # Go through each of the infected people and try to infected people nearby
+    # Correr cada lugar da sala e ver se ta contaminado
         for x in infected_people:
             infection_circle = x.location_x - radius
             if x.days >= time_to_spread:
@@ -123,7 +148,9 @@ def infecting():
                         if i.location_x > x.location_x + radius:
                             break
         healing()
-
+        
+       
+        
 def main():
     create_room()
     infecting()
