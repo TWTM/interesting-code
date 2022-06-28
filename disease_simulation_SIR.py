2df385081aa86d3b357c7fa4dd6daca261e7a566
TWTM <52894737+TWTM@users.noreply.github.com>
2022-06-28 20:43:40 -0300

    Update disease_simulation_SIR.py

--- disease_simulation_SIR.py
+++ disease_simulation_SIR.py
@@ -4,13 +4,6 @@ import time
 
 tempo = time.time()
 
-# Changeable Parameters
-total_people = 2000
-radius = 8
-days_until_healed = 6
-time_to_spread = 3
-initial_prob = 0.2
-
 # Used to draw the graph
 n_infected = 0
 n_healthy = 0
@@ -19,7 +12,7 @@ recovered = []
 infected = []
 healthy = []
 
-# Necessary variables
+# Used to draw the graph
 total_days = []
 infected_people = []
 healthy_people = []
@@ -64,7 +57,7 @@ def printgrid():
         else:
             plt.scatter(recovered_x, recovered_y, color = 'blue', s = 3)
             plt.pause(0.0001)
-    plt.title(days)
+    plt.title(label= days)
     plt.xlabel(len(infected_people))
 
 
@@ -80,12 +73,12 @@ def draw():
     plt.show()
 
 # Criando a sala
-def create_room():
+def create_room(infected_proportion, total_people):
     global infected_people, healthy_people
     for x in range(total_people):
         random_x = np.random.uniform(0,total_people*0.1)
         random_y = np.random.uniform(0,total_people*0.1)
-        if x % 100 == 0:
+        if x % (total_people * infected_proportion) == 0:
             infected_people.append(person('G', 0, 1, random_x ,random_y,x))
             continue    
         healthy_people.append(person('S', 0, 1, random_x ,random_y,x))
@@ -93,8 +86,8 @@ def create_room():
     healthy_people = sorted(healthy_people, key=lambda x: x.location_x)
     infected_people = sorted(infected_people, key=lambda x: x.location_x)
 
-def healing():
-    global n_infected, n_healthy, n_recovered, recovered ,infected ,healthy, total_days, infected_people, healthy_people, days
+def healing(days_until_healed):
+    global days, infected_people, healthy_people, recovered_people
     # Checking the days since he got the desease
     for i in infected_people:
         # Checking the days since he got the desease
@@ -110,11 +103,10 @@ def healing():
             # if he has been infected 4 times he now is categorized as immune and cant be infected again
             if i.immunity == 4:
                 i.state = 'R'
-                n_recovered += 1
                 healthy_people.remove(i)
                 recovered_people.append(i)
 
-    recovered.append(n_recovered)
+    recovered.append(len(recovered_people))
     
     days += 1              
     total_days.append(days)
@@ -127,33 +119,33 @@ def healing():
     healthy_people = sorted(healthy_people, key=lambda x: x.location_x)
     infected_people = sorted(infected_people, key=lambda x: x.location_x)
 
-def infecting():
+def infecting(radius, time_to_spread, initial_prob,days_until_healed):
     while (len(infected_people) != 0):
     # Correr cada lugar da sala e ver se ta contaminado
         for x in infected_people:
-            infection_circle = x.location_x - radius
+            infection_line = x.location_x - radius
             if x.days >= time_to_spread:
                 # Pegando o lugar das pessoas que possivelmente possam ser contaminadas
                 for i in healthy_people:
-                    if i.location_x < infection_circle:
+                    if i.location_x < infection_line:
                         continue
                     elif ((i.location_x - x.location_x)**2 + (i.location_y - x.location_y)**2 <= radius**2):
-                        chance_of_infection = np.random.uniform(0,1)
                         # 30% de chance de ser infectado
-                        if chance_of_infection <= initial_prob * 1/i.immunity:
+                        if np.random.uniform(0,1) <= initial_prob * 1/i.immunity:
                             i.state = 'G'
                             healthy_people.remove(i)
                             infected_people.append(i)
                     else:
                         if i.location_x > x.location_x + radius:
                             break
-        healing()
+        healing(days_until_healed)
         
-def main():
-    create_room()
-    infecting()
-    draw()
+        
+def run_simulation(total_people, radius, days_until_healed, time_to_spread, initial_prob, infected_proportion):
+    create_room(infected_proportion, total_people)
+    infecting(radius, time_to_spread, initial_prob, days_until_healed)
     print(days)
     print(time.time() - tempo)
+    draw()
 
-main()
+run_simulation(total_people= 2000, radius=8, days_until_healed= 6, time_to_spread= 3, initial_prob=0.2, infected_proportion= 1/10)
